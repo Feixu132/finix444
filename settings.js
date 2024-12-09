ffc77c60d23415f61d58cb9fa91807726f0b30e8
Mee Mee <ai09did@gmail.com>
2024-12-09 19:42:33 +0800

    first commit

---
+++ settings.js
@@ -0,0 +1,11 @@
+// OBEY COMMANDMENTS
+
+global.prefa = ['','!','.',',','🐤','🗿'] //NOT CHANGE
+global.owner = ['6285947535179'] //OWNER
+global.botname = 'Finix 444' //BOT NAME
+global.versisc = "4.4.4" //NOT CHANGE
+global.baileys1 = require('@whiskeysockets/baileys') //NOT CHANGE
+global.creator = "AlwaysXdamr66" //NOT CHANGE
+global.packname = "Sticker By" //OPSIONAL
+global.author = "alwaysXdmar66" //OPSIONAL
+global.simbol = "✇" // SIMBOL MENU
